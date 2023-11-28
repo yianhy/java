c66b0033eb9b23c74662129d71f66a8832301732
yian <yiankonlie@gmail.com>
2023-11-28 20:31:28 +0800

    Initial commit

---
+++ javase/src/main/java/priv/yian/javase/simpledatetype/SimpleDateType.java
@@ -0,0 +1,13 @@
+package priv.yian.javase.simpledatetype;
+
+/**
+ * @author yian
+ * @date 2023/11/28 20:26
+ * Description:
+ */
+public class SimpleDateType {
+    public static void main(String[] args) {
+        byte a = 3;
+
+    }
+}
